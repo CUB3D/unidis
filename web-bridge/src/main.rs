7ba0bbd997a4f6167837521874cea79582e810d0
CUB3D <callumthom11@gmail.com>
2026-08-21 17:10:55 +0000

    Works on wasm now

--- web-bridge/src/main.rs
+++ web-bridge/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::BTreeMap;
 use std::sync::LazyLock;
 use tracing::level_filters::LevelFilter;
 use libundis::{ARCHES, UniDis, UnidisArch};
+use anyhow::Context;
 
 pub fn get_arch_map() -> BTreeMap<String, UnidisArch> {
     tracing::info!("get_arch_map(_)");
@@ -15,13 +16,13 @@ pub fn get_arch_map() -> BTreeMap<String, UnidisArch> {
 
 const ARCH_MAP: LazyLock<BTreeMap<String, UnidisArch>> = LazyLock::new(get_arch_map);
 
-pub fn guess_arch(x: &[u8]) -> UnidisArch {
+pub fn guess_arch(x: &[u8]) -> anyhow::Result<UnidisArch> {
     tracing::info!("guess_arch(_)");
 
     let mut res = (0, UnidisArch::ArmV8Le);
     for a in ARCH_MAP.values() {
-        let dis = UniDis::new_arch(*a).expect("new arch fail");
-        let mut dis = dis.dissassembler(x.to_vec(), 0).expect("dis fail");
+        let dis = UniDis::new_arch(*a).context("new arch fail")?;
+        let mut dis = dis.dissassembler(x.to_vec(), 0).context("dis fail")?;
 
         let mut c = 0;
         while let Ok(Some(i)) = dis.next_instruction() {
@@ -32,21 +33,7 @@ pub fn guess_arch(x: &[u8]) -> UnidisArch {
         }
     }
 
-    res.1
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn guess_arch_bridge(x: *mut u8, len: usize) -> usize {
-    tracing::info!("guess_arch_bridge({}, {len})", x as usize);
-    if x.is_null() {
-        return 1;
-    }
-    let slc = unsafe { std::slice::from_raw_parts(x, len) };
-    // print!("Guess arch");
-    let a = guess_arch(slc);
-    println!("{:?}", a);
-
-    return 0;
+    Ok(res.1)
 }
 
 #[unsafe(no_mangle)]
@@ -54,6 +41,268 @@ pub extern "C" fn alloc_bytes(size: usize, align: usize) -> *mut u8 {
     tracing::info!("alloc_bytes(size: {size}, align: {align})");
     let layout = std::alloc::Layout::from_size_align(size, align).expect("Alloc fail");
     unsafe { std::alloc::alloc(layout) }
+}
+
+pub fn load() {
+    tracing::info!("load()");
+    for arch in ARCH_MAP.keys() {
+        emscripten_functions::emscripten::run_script(
+            format!(
+                r##"
+                    const opt1 = document.createElement("option");
+                    opt1.value = "{}";
+                    opt1.text = "{}";
+                    document.getElementById("arch").add(opt1, null);
+                "##,
+                arch,
+                arch,
+            )
+        );
+    }
+
+    tracing::info!("Load done");
+}
+
+pub fn dissassemble_impl(input_data: &str, base_addr: &str, arch: &str, include_addr: bool, include_bytes: bool) -> anyhow::Result<String> {
+    // Parse base addr
+    let base_addr = if base_addr.starts_with("0x") {
+        u64::from_str_radix(&base_addr[2..], 16)
+    } else {
+        base_addr.parse::<u64>()
+    };
+    let base_addr = base_addr.context("Failed to parse number")?;
+
+    let x = hex::decode(input_data.replace(" ", "")).context("Hex decode fail")?;
+
+    let arch = if arch == "Guess for me" {
+        match guess_arch(&x) {
+            Ok(a) => a,
+            Err(e) => {
+                return Err(anyhow::anyhow!("Failed to guess arch: {:?}", e));
+            }
+        }
+    } else {
+        match ARCH_MAP.get(arch) {
+            Some(v) => *v,
+            None => return Err(anyhow::anyhow!("Bad arch")),
+        }
+    };
+
+    let mut out = String::new();
+
+    let y = UniDis::new_arch(arch).context("Failed to create arch")?;
+    let mut x = y.dissassembler(x, 0).context("Failed to dis")?;
+
+    while let Ok(Some(c)) = x.next_instruction() {
+        if include_addr {
+            out.push_str(&format!("{:08x}: ", c.address() + base_addr));
+        }
+        if include_bytes {
+            for b in c.bytes() {
+                out.push_str(&format!("{:02X} ", b));
+            }
+        }
+        out.push_str("        ");
+        out.push_str(&c.memonic());
+        let mut args = c.args().into_iter().peekable();
+        if args.peek().is_some() {
+            out.push_str("    ");
+        }
+        while let Some(arg) = args.next() {
+            out.push_str(&arg);
+            if args.peek().is_some() {
+                out.push_str(", ");
+            }
+        }
+        out.push('\n');
+    }
+    println!("out = {out}");
+
+    Ok(out)
+}
+
+pub fn assemble_impl(input_data: &str, base_addr: &str, arch: &str) -> anyhow::Result<String> {
+    // Parse base addr
+    let base_addr = if base_addr.starts_with("0x") {
+        u64::from_str_radix(&base_addr[2..], 16)
+    } else {
+        base_addr.parse::<u64>()
+    };
+    let base_addr = base_addr.context("Failed to parse number")?;
+    
+    use keystone_engine::*;
+
+    let (arch, mode) = if arch == "Guess for me" {
+        return Err(anyhow::anyhow!("I can't guess what you want to assemble"));
+    } else {
+        match ARCH_MAP.get(arch) {
+            Some(v) => match v {
+                UnidisArch::X86_64 => (Arch::X86, Mode::MODE_64),
+                UnidisArch::ArmV8Le => (Arch::ARM, Mode::V8),
+                UnidisArch::AArch64 => (Arch::ARM64, Mode::LITTLE_ENDIAN),
+                UnidisArch::Hexagon => (Arch::HEXAGON, Mode::LITTLE_ENDIAN),
+                _ => {
+                    return Err(anyhow::anyhow!("Unsupported architecture"));
+                },
+            },
+            None => {
+                return Err(anyhow::anyhow!("Arch not found"));
+            }
+        }
+    };
+
+    let asm = match Keystone::new(arch, mode) {
+        Ok(v) => v,
+        Err(e) => return Err(anyhow::anyhow!("Keystone error: {:?}", e)),
+    };
+
+    let res = match asm.asm(input_data.to_string(), base_addr) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("{:?}", e);
+            return Err(anyhow::anyhow!("Failed to assemble: {:?}", e));
+        },
+    };
+
+    let mut out = String::new();
+    for c in res.bytes {
+        out.push_str(&format!("{:02X} ", c));
+    }
+    let out = out.trim().to_string();
+    println!("out = {out:?}");
+
+    // Returning dies here, probably unsoundness in keystone causing a fault on dropping the engine. Lets just do this for now
+    emscripten_functions::emscripten::run_script(
+        format!(
+            r##"
+                document.getElementById("output").value = "{}";
+            "##,
+            out.replace("\n", "\\n"),
+        )
+    );
+
+    Ok(out)
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn assemble(
+    data_ptr: *mut u8,
+    data_len: usize,
+
+    base_addr_ptr: *mut u8,
+    base_addr_len: usize,
+
+    arch_ptr: *mut u8,
+    arch_len: usize,
+) -> i32 {
+    tracing::info!("assemble({}, {data_len})", data_ptr as usize);
+    if data_ptr.is_null() || base_addr_ptr.is_null() || arch_ptr.is_null() {
+        return 1;
+    }
+    let input_data: &[u8] = unsafe { std::slice::from_raw_parts(data_ptr, data_len) };
+    let input_data = String::from_utf8(input_data.to_vec()).expect("Bad input_data");
+    let base_addr = unsafe { std::slice::from_raw_parts(base_addr_ptr, base_addr_len) };
+    let base_addr = String::from_utf8(base_addr.to_vec()).expect("Bad base_addr");
+    let arch = unsafe { std::slice::from_raw_parts(arch_ptr, arch_len) };
+    let arch = String::from_utf8(arch.to_vec()).expect("Bad arch");
+
+    emscripten_functions::emscripten::run_script(
+        format!(
+            r##"
+                document.getElementById("output_title").textContent = "Assembly Output";
+            "##,
+        )
+    );
+
+    match assemble_impl(&input_data, &base_addr, &arch) {
+        Ok(_out) => {
+            // emscripten_functions::emscripten::run_script(
+            //     format!(
+            //         r##"
+            //             document.getElementById("output").value = "{}";
+            //         "##,
+            //         out.replace("\n", "\\n"),
+            //     )
+            // );
+            0
+        }
+        Err(e) => {
+            println!("Assembly error: {:?}", e);
+            emscripten_functions::emscripten::run_script(
+                format!(
+                    r##"
+                        document.getElementById("output").value = "{}";
+                    "##,
+                    e.to_string().replace("\n", "\\n"),
+                )
+            );
+            1
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn dissassemble(
+    data_ptr: *mut u8,
+    data_len: usize,
+
+    base_addr_ptr: *mut u8,
+    base_addr_len: usize,
+
+    arch_ptr: *mut u8,
+    arch_len: usize,
+
+    include_addr: i32,
+    include_bytes: i32,
+) -> i32 {
+    tracing::info!("dissassemble({}, {data_len})", data_ptr as usize);
+    if data_ptr.is_null() || base_addr_ptr.is_null() || arch_ptr.is_null() {
+        return 1;
+    }
+    let input_data: &[u8] = unsafe { std::slice::from_raw_parts(data_ptr, data_len) };
+    let input_data = String::from_utf8(input_data.to_vec()).expect("Bad input_data");
+    let base_addr = unsafe { std::slice::from_raw_parts(base_addr_ptr, base_addr_len) };
+    let base_addr = String::from_utf8(base_addr.to_vec()).expect("Bad base_addr");
+    let arch = unsafe { std::slice::from_raw_parts(arch_ptr, arch_len) };
+    let arch = String::from_utf8(arch.to_vec()).expect("Bad arch");
+
+    emscripten_functions::emscripten::run_script(
+        format!(
+            r##"
+                document.getElementById("output_title").textContent = "Disassembly Output";
+            "##,
+        )
+    );
+
+    match dissassemble_impl(&input_data, &base_addr, &arch, include_addr != 0, include_bytes != 0) {
+        Ok(out) => {
+            println!("Disas ok");
+            emscripten_functions::emscripten::run_script(
+                format!(
+                    r##"
+                        document.getElementById("output").value = "{}";
+                    "##,
+                    out.replace("\n", "\\n"),
+                )
+            );
+
+            0
+        }
+        Err(e) => {
+            println!("Disassembly error: {:?}", e);
+            emscripten_functions::emscripten::run_script(
+                format!(
+                    r##"
+                        document.getElementById("output").value = "{}";
+                    "##,
+                    e.to_string().replace("\n", "\\n"),
+                )
+            );
+
+            1
+        }
+    }
+
 }
 
 pub fn main() {
@@ -67,12 +316,11 @@ pub fn main() {
         .init();
     tracing::info!("starting up");
     println!("Started");
-    unsafe {
-        std::env::set_var("RUST_BACKTRACE", "full");
-    }
+    load();
 }
 
 #[test]
 pub fn foo() {
     guess_arch(b"Test");
+    assemble_impl("nop", "0x0", "X86_64::LE::default");
 }
